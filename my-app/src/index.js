6468defd571305ac764d48ebe2c4b34ec865c94e
skxeve <skxeve@gmail.com>
2018-12-01 13:42:06 +0900

    Showing the Past Moves script.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -123,6 +123,17 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     const status = winner ?
       'Winner: ' + winner :
       'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
@@ -137,7 +148,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
